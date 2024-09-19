4e8df3d6d258140cdfb85077ff81a933c14c0845
saqibhaneef <muhammadsaqibhanif80@gmail.com>
2024-09-20 00:13:30 +0500

    Configure Entity framework and sql in .net aspire api

---
+++ AspireDemo/AspireDemo.Api/Data/Seeding/SeedData.cs
@@ -0,0 +1,36 @@
+﻿namespace AspireDemo.Api.Data.Seeder
+{
+    public static class SeedData
+    {
+        public static IEnumerable<WeatherForecast> GetProducts()
+        {
+            return new List<WeatherForecast>
+            {
+                new WeatherForecast()
+                {
+                    Date=DateOnly.MaxValue,
+                    Summary="Summary 1",
+                    TemperatureC=32,                    
+                },
+                new WeatherForecast()
+                {
+                    Date=DateOnly.MaxValue,
+                    Summary="Summary 2",
+                    TemperatureC=30,
+                },
+                new WeatherForecast()
+                {
+                    Date=DateOnly.MaxValue,
+                    Summary="Summary 3",
+                    TemperatureC=38,
+                },
+                new WeatherForecast()
+                {
+                    Date=DateOnly.MaxValue,
+                    Summary="Summary 4",
+                    TemperatureC=25,
+                }
+            };
+        }        
+    }
+}
--- AspireDemo/AspireDemo/Program.cs
+++ AspireDemo/AspireDemo/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var connectionString = builder.AddConnectionString("ShoeMoneyDb");
 
 var password = builder.AddParameter("password");
 
@@ -6,9 +10,11 @@ var rabbit = builder.AddRabbitMQ("messaging", password: password)
     .WithManagementPlugin();
 
 var api = builder.AddProject<Projects.AspireDemo_Api>("aspiredemo-api")
+    //.WithEnvironment("ConnectionStrings:ShoeMoneyDb", builder.Configuration.GetConnectionString("ShoeMoneyDb"))
+    .WithReference(connectionString)
     .WithReference(rabbit);
 
-builder.AddProject<Projects.AspireDemo_Mvc>("aspiredemo-mvc")
+builder.AddProject<Projects.AspireDemo_Mvc>("aspiredemo-mvc")    
     .WithReference(api)
     .WithReference(rabbit);
 
